511700d6d0c180fd04fad16ce11358bc33bfbdee
xtvlw <bernardopereira01@pm.me>
2022-11-02 16:51:43 -0300

    adding navbar style

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -5,8 +5,10 @@ const Header = () => {
     <section>
         <h1>Doggo images</h1>
         <ul>
-            <li>Why?</li>
-            <li>Get the code</li>
+            <li onClick={() => {
+                alert("I Like Doggos!")
+            }}>Why?</li>
+            <li><a target="blank" href='https://github.com/xtvlw/Doggo-img'>Get the code</a></li>
         </ul>
     </section>
     )
